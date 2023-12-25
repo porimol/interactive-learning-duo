c134094484cd606bd3027b3062b0f14da6f3dc30
Porimol Chandro <porimol.chandro@roche.com>
2023-12-25 22:48:21 +0100

    q&a insert and retrive from DB

---
+++ app/db/qa.py
@@ -0,0 +1,56 @@
+from sqlite3 import Error
+from app.db.sql import connect_db
+
+
+def insert_question(user_id, question, answer, question_category, created_at):
+    """
+    Insert a new question into the database.
+
+    Args:
+        user_id (int): The ID of the user who asked the question.
+        question (str): The question text.
+        answer (str): The answer to the question.
+        question_category (str): The category of the question.
+        created_at (str): The timestamp when the question was created.
+
+    Returns:
+        bool: True if the question was successfully inserted, False otherwise.
+    """
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO questions_answers(user_id, question, answer, question_category, created_at) VALUES (?, ?, ?, ?, ?)",
+                    (user_id, question, answer, question_category, created_at))
+        conn.commit()
+        conn.close()
+        return True
+    except Error as error:
+        print("Error while connecting to sqlite", error)
+        return error
+
+
+def get_todays_qa_by_userid(user_id, created_at):
+    """
+    Retrieve a user from the database by their ID.
+
+    Args:
+        id (int): The ID of the user to retrieve.
+
+    Returns:
+        dict: A dictionary containing the user's information, with column names as keys.
+
+    Raises:
+        sqlite3.Error: If there is an error while connecting to the SQLite database.
+    """
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM questions_answers WHERE user_id=? AND DATE(created_at)=? ORDER BY created_at DESC", (user_id, created_at,))
+        # columns = [description[0] for description in cursor.description]
+        user = cursor.fetchall()
+        conn.close()
+        # user = dict(zip(columns, user))
+        return user
+    except Error as error:
+        print("Error while connecting to sqlite", error)
+        return error
--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Request, Depends
-from fastapi.responses import HTMLResponse
+from datetime import datetime
+from fastapi import APIRouter, Request, Depends, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from app.dependencies import is_authorized
+from app.db.qa import insert_question, get_todays_qa_by_userid
 
 router = APIRouter(prefix="/chatbot")
 router.dependencies = [Depends(is_authorized)]
@@ -11,9 +13,55 @@ templates = Jinja2Templates(directory="app/templates")
 @router.get("/qa", response_class=HTMLResponse)
 async def chat_home(request: Request):
     """
-    A function that handles the root route.
+    This function handles the chat home page request.
+
+    Args:
+        request (Request): The incoming request object.
 
     Returns:
-        TemplateResponse: The response containing the rendered index.html template.
+        TemplateResponse: The template response containing the chatbot.html template, request object, and questions.
     """
-    return templates.TemplateResponse("chatbot.html", {"request": request})
+    today_date = datetime.now().strftime("%Y-%m-%d")
+    user_id = request.session["USER_ID"]
+    questions = get_todays_qa_by_userid(user_id, today_date)
+    return templates.TemplateResponse(
+        "chatbot.html", {
+            "request": request,
+            "questions": questions,
+            # "error": error
+        })
+
+
+@router.post("/qa", response_class=HTMLResponse)
+async def question_answer(request: Request):
+    """
+    Handles POST requests to the "/qa" route. This function takes a user's question, 
+    validates the form data, and inserts the question into the database.
+
+    Args:
+        request (Request): The request object containing all the HTTP information.
+
+    Returns:
+        RedirectResponse: A redirection to "/chatbot/qa" if the question was successfully inserted.
+        If there were any empty fields in the form or if the insertion failed, it redirects to 
+        "/chatbot/qa" with an appropriate error message.
+    """
+
+    form = await request.form()
+    empty_fields = [field.replace("_", " ") for field, value in form.items() if not value]
+    
+    if empty_fields:
+        message = f"Empty fields: {', '.join(empty_fields)}"
+        return RedirectResponse(f"/chatbot/qa?error={message}", status_code=status.HTTP_302_FOUND)
+    
+    user_id = request.session["USER_ID"]
+    question = form.get("question")
+    answer = ""
+    question_category = "python"
+    today_date = datetime.now().strftime("%Y-%m-%d %I:%M")
+    insert_rsp = insert_question(user_id, question, answer, question_category, today_date)
+    if insert_rsp:
+        return RedirectResponse("/chatbot/qa", status_code=status.HTTP_302_FOUND)
+    
+    return RedirectResponse("/chatbot/qa?error=Something went wrong", status_code=status.HTTP_302_FOUND)
+
